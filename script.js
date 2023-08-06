9a61b2d47b9267fc526c3c352a96f400f73bedfe
Breno <onerbreno@outlook.com>
2023-08-06 14:27:58 -0300

    feat Add calculator display and result visualization

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
-const calcButton = document.querySelector('.calc-button');
-const resultParagraph = document.querySelector('.result p');
+const form = document.querySelector('.form-container');
+const calcContainer = document.querySelector('.calc-container');
+const calcText = document.querySelector('.calc-container p');
+const resultContainer = document.querySelector('.result-container');
+const resultBadge = document.querySelector('.result-badge');
+const result = document.querySelector('.result-calc');
 
-calcButton.addEventListener('click', calculateIMC);
+function getImcStatus(imc) {
+  let statusClass = 'status-alert';
+  let message = 'Obesidade';
+
+  if (imc <= 18.5) {
+    message = 'Abaixo do peso';
+  } else if (imc <= 24.9) {
+    statusClass = 'status-ok';
+    message = 'Peso ideal';
+  } else if (imc <= 29.9) {
+    message = 'Sobrepeso';
+  }
+
+  return { statusClass, message };
+}
 
 function calculateIMC(e) {
-  e.preventDefault()
+  e.preventDefault();
+
   const weightInput = document.getElementById('weight');
   const heightInput = document.getElementById('height');
 
   const weight = parseFloat(weightInput.value);
-  const height = parseFloat(heightInput.value);
+  const height = parseFloat(heightInput.value) / 100;
 
-  if (isNaN(weight) || isNaN(height) || height <= 0) {
-    resultParagraph.textContent = "Insira valores válidos de peso e altura.";
+
+  if (isNaN(weight) || isNaN(height) || height <= 0 || weight <= 0) {
+    showError('Insira valores válidos de peso e altura.');
     return;
   }
 
-  const imc = (weight / (height * height)).toFixed(2).replace('.', ',');
+  const imc = weight / (height * height);
+  const imcFormatted = imc.toFixed(2).replace('.', ',');
 
-  resultParagraph.textContent = `${weight}kg * ${height}m = ${imc}`;
+  const {statusClass, message} = getImcStatus(imc)
+
+  updateCalcText(weight, height, imcFormatted);
+  updateResultElements(statusClass, message, imcFormatted);
 }
+
+function showError(message) {
+  calcText.textContent = message;
+  calcContainer.classList.remove('hidden');
+}
+
+function updateCalcText(weight, height, imcFormatted) {
+  String(height).replace('.', ',')
+  String(weight).replace('.', ',')
+  calcContainer.classList.remove('hidden');
+
+  calcText.textContent = `${weight}kg / (${height}m * ${height}m) = ${imcFormatted}`;
+}
+
+function updateResultElements(statusClass, message, imcFormatted) {
+  resultBadge.textContent = message;
+  result.textContent = imcFormatted;
+
+  resultContainer.setAttribute('data-status', statusClass);
+  resultBadge.setAttribute('data-status', statusClass);
+  result.setAttribute('data-status', statusClass);
+  resultContainer.classList.remove('hidden');
+}
+
+form.addEventListener('submit', calculateIMC);
